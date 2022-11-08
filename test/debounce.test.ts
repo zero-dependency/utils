0e87426e38b3e864bcbccaf2ad1602792a9b6299
Vitalij Ryndin <sys@crashmax.ru>
2022-11-09 02:52:36 +0800

    test: add debounce

---
+++ test/debounce.test.ts
@@ -0,0 +1,48 @@
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
+import { debounce } from '../src/index.js'
+
+describe('debounce', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  test('should be defined', () => {
+    expect(debounce).toBeDefined()
+  })
+
+  test('should be defined closure', () => {
+    const debouncedFn = vi.fn()
+    const debounceFn = debounce(debouncedFn, 1000)
+
+    expect(debounceFn).toBeDefined()
+  })
+
+  test('execute just once', () => {
+    const debouncedFn = vi.fn()
+    const debounceFn = debounce(debouncedFn, 1000)
+
+    debounceFn()
+    debounceFn()
+    debounceFn()
+    vi.runAllTimers()
+
+    expect(debouncedFn).toBeCalledTimes(1)
+  })
+
+  test('apply arguments', () => {
+    const debouncedFn = vi.fn((key: number, value: string) => {
+      expect(key).toBe(0)
+      expect(value).toBe('foo')
+    })
+
+    const debounceFn = debounce(debouncedFn, 1000)
+    debounceFn(0, 'foo')
+    vi.runAllTimers()
+
+    expect(debouncedFn).toBeCalledTimes(1)
+  })
+})
