1c6df501a94e6aa39c3560ea62e65c4889b40551
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-04-11 16:50:06 +0800

    chore: moved `randomNum` to numbers

--- src/number.ts
+++ src/number.ts
@@ -7,3 +7,13 @@ export function addZero(num: number): string {
   num = Math.abs(num)
   return num > 9 ? `${num}` : `0${num}`
 }
+
+/**
+ * Returns a random number between min (inclusive) and max (inclusive)
+ * @param min min value
+ * @param max max value
+ * @returns random number between min and max
+ */
+export function randomNum(min = 0, max = Number.MAX_SAFE_INTEGER - 1): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
