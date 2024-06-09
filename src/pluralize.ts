76cf3d2b211f38dc49bf868dcf1cd6af26060f4c
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-06-09 22:02:36 +0800

    feat(pluralize): add count to tagged literale template

--- src/pluralize.ts
+++ src/pluralize.ts
@@ -1,3 +1,5 @@
+export type PluralizeToken = (string[] | ((total: number) => string | number))[]
+
 /**
  * Returns a function that takes a `TemplateStringsArray` and an array of tokens and
  * returns a string by concatenating the interpolated values of the strings and tokens.
@@ -5,19 +7,24 @@
  * @param {number} total
  * The total number of items.
  *
- * @returns {(strings: TemplateStringsArray, ...tokens: string[][]) => string}
+ * @returns {(strings: TemplateStringsArray, ...tokens: PluralizeToken) => string}
  * The function that takes a `TemplateStringsArray` and an array of tokens and returns the final string.
  *
  * @example
- * pluralize(2)`packag${['e', 'es']} agenc${['y', 'ies']}` // packages agencies
+ * pluralize(2)`${(count) => count} packag${['e', 'es']}` // 2 packages
  */
 export function pluralize(
   total: number
-): (strings: TemplateStringsArray, ...tokens: string[][]) => string {
+): (strings: TemplateStringsArray, ...tokens: PluralizeToken) => string {
   return (strings, ...tokens) => {
     return strings.reduce((acc, item, index) => {
-      const token = getToken(total, tokens[index - 1]!)
-      return acc + token + item
+      const currentToken = tokens[index - 1]!
+
+      if (typeof currentToken === 'function') {
+        return acc + currentToken(total) + item
+      }
+
+      return acc + pluralizeToken(total, currentToken) + item
     })
   }
 }
@@ -25,13 +32,17 @@ export function pluralize(
 // prettier-ignore
 const cases = [2, 0, 1, 1, 1, 2]
 
-function getToken(total: number, tokens: string[]): string {
+function pluralizeToken(total: number, tokens: string[]): string {
   total = Math.abs(total)
 
   const index =
     total % 100 > 4 && total % 100 < 20
       ? 2
       : cases[total % 10 < 5 ? total % 10 : 5]!
+
+  if (index === 2 && tokens.length === 2) {
+    return tokens[1]!
+  }
 
   return tokens[index]!
 }
--- test/pluralize.test.ts
+++ test/pluralize.test.ts
@@ -7,25 +7,40 @@ describe('pluralize', () => {
     expect(pluralize).toBeDefined()
   })
 
-  // prettier-ignore
-  const tokens = ['е', 'я', 'й']
-  const pluralizeTasks = (count: number): string => {
-    return pluralize(count)`задани${tokens}`
+  function pluralizeTasks(count: number): string {
+    return pluralize(count)`задани${[
+      'е',
+      'я',
+      'й'
+    ]}`
   }
 
   test('should pluralize tasks count', () => {
+    const packageTokens = ['e', 'es']
+    const agencyTokens = ['y', 'ies']
+
     expect(pluralizeTasks(1)).toBe('задание')
     expect(pluralizeTasks(2)).toBe('задания')
     expect(pluralizeTasks(5)).toBe('заданий')
-    expect(pluralize(1)`packag${['e', 'es']} agenc${['y', 'ies']}`).toBe(
+    expect(pluralizeTasks(10)).toBe('заданий')
+    expect(pluralize(1)`packag${packageTokens} agenc${agencyTokens}`).toBe(
       'package agency'
     )
-    expect(pluralize(2)`packag${['e', 'es']} agenc${['y', 'ies']}`).toBe(
+    expect(pluralize(2)`packag${packageTokens} agenc${agencyTokens}`).toBe(
+      'packages agencies'
+    )
+    expect(pluralize(10)`packag${packageTokens} agenc${agencyTokens}`).toBe(
       'packages agencies'
     )
   })
 
   test('negative total', () => {
     expect(pluralizeTasks(-999)).toBe('заданий')
+  })
+
+  test('should be return current count from template tagged string', () => {
+    expect(pluralize(10)`${(count) => count} packag${['e', 'es']}`).toBe(
+      '10 packages'
+    )
   })
 })
